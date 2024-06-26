fe8efe39cfa095f09a2d6d002e44a2e03a6d2697
Jai Carey <31185022+goodthingscomein@users.noreply.github.com>
2024-06-26 22:11:10 +1000

    Implemented the Drawing Lines tutorial on the ThreeJS site

--- main.js
+++ main.js
@@ -4,21 +4,39 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 5);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
 camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-}
+// Create a geometry with some points
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
 
-renderer.setAnimationLoop(animate);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+// Now that we have points for 2 lines, and we also have a material, we can put them together to form a line
+const line = new THREE.Line(geometry, material);
+
+// Finally, add the line to the scene and call render
+scene.add(line);
+renderer.render(scene, camera);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// function animate() {
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+//     renderer.render(scene, camera);
+// }
+
+// renderer.setAnimationLoop(animate);
